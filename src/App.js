8c4eb8c9c3f365faf1d61734e8236b44315f7fbf
Rufji <mahmudrufai05@gmail.com>
2023-04-03 09:25:32 -0700

    created pages and routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import Signin from './pages/Signin';
+import SignUp from './pages/SignUp';
+import ForgotPass from './pages/ForgotPass';
+import Offers from './pages/Offers';
+
 
 function App() {
   return (
     <>
-      <h1>hello world</h1>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/Profile' element={<Profile />} />
+          <Route path='/Signin' element={<Signin />} />
+          <Route path='/SignUp' element={<SignUp />} />
+          <Route path='/ForgotPass' element={<ForgotPass />} />
+          <Route path='/Offers' element={<Offers/>} />
+        </Routes>
+      </Router>
     </>
   );
 }
